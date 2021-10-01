e538ddd2977c101be62cc4bb9b733a784c2755ca
kojo <kgyamfi@iastate.edu>
2021-10-01 16:41:42 -0500

    Initial Commit

---
+++ WellnessScout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WellnessScout
+//
+//  Created by Sharma, Anuj [CCE E] on 10/1/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
